af39dcc29fe12e90e69342ebcc26cc8899572c2b
shelton15 <sheltoniyassa@gmail.com>
2023-06-07 12:10:51 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import LoginPage from './components/login page/loginpage';
 import Home from './components/HomePage/Home';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
       <LoginPage/>
       <Home/>
+      <NavBar/>
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import React, { Component} from "react";
+import "./NavBar.css"
+
+class NavBar extends Component{
+    constructor(props) {
+        super(props);
+        this.state ={ }
+    }
+    render() {
+        return (
+            <div>Hello from Itor</div>
+        );
+    }
+}
+
+export default NavBar;
